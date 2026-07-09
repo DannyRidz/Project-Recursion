d82d33858cf4462cf7eb37b2d9d11596b5873cdb
DannyRidz <danny.ridz93@gmail.com>
2026-07-09 20:24:12 +0800

    implement merge function and fix mergeSort return statement

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,24 @@
+function merge(leftArray, rightArray) {
+  const sortedArray = [];
+
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+    if (leftArray[leftIndex] < rightArray[rightIndex]) {
+      sortedArray.push(leftArray[leftIndex]);
+      leftIndex++;
+    } else {
+      sortedArray.push(rightArray[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return sortedArray
+    .concat(leftArray.slice(leftIndex))
+    .concat(rightArray.slice(rightIndex));
+}
+
 function mergeSort(array) {
   if (array.length <= 1) {
     return array;
@@ -10,5 +31,8 @@ function mergeSort(array) {
   const sortedLeft = mergeSort(leftHalf);
   const sortedRight = mergeSort(rightHalf);
 
-  return mergeSort(sortedLeft, sortedRight);
+  return merge(sortedLeft, sortedRight);
 }
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
